43f0dd73863090dbb9172b2554be5cc7c42ee244
Mostafa <darth.mostafa@gmail.com>
2018-09-16 15:15:35 +0430

    watum just got initialized

---
+++ jhang.m
@@ -0,0 +1,19 @@
+function Ex = jhang( b, y, z, s, flow, u )
+%% "Jhang 2015" Longitudinal dispersion coefficient
+% This function Zeng and Huai 2015"  LDC
+% use meter and qubic meter as your dimentions
+a   = y .* ( b + z .* y);
+T   =b + 2 .* z .* y;
+p   =b + 2 .* y .* sqrt(1+z .^ 2);
+r   =a  ./  p;
+D   =a  ./  T; %#ok
+switch nargin   
+    case 5
+    u   = flow ./ a;
+end
+g   =9.81;
+w   =a  ./  y;
+ustar   =sqrt(g .* r .* s);
+Ex      = y .* ustar .* 3.563 .* ((u ./  (sqrt(g .* y))) .^ -0.4117) .* ((w  ./  y) .^ 0.6776) .* ((u  ./  ustar) .^ 1.0132);
+end
+
