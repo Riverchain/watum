b7c941c54ab0c68f6cb63fe56c8ae0be200e8122
Mostafa <darth.mostafa@gmail.com>
2018-09-19 21:51:25 +0430

    some little modifications in jhabg ldc

--- jhang.m
+++ jhang.m
@@ -1,6 +1,6 @@
 function Ex = jhang( b, y, z, s, flow, u )
 %% "Jhang 2015" Longitudinal dispersion coefficient
-% This function Zeng and Huai 2015"  LDC
+% This function Zeng and Huai 2015  LDC
 % use meter and qubic meter as your dimentions
 a   = y .* ( b + z .* y);
 T   =b + 2 .* z .* y;
@@ -16,4 +16,3 @@ w   =a  ./  y;
 ustar   =sqrt(g .* r .* s);
 Ex      = y .* ustar .* 3.563 .* ((u ./  (sqrt(g .* y))) .^ -0.4117) .* ((w  ./  y) .^ 0.6776) .* ((u  ./  ustar) .^ 1.0132);
 end
-
